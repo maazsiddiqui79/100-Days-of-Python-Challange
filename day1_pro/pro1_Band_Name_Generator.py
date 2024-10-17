310b9c74566336f0110beef92f60b92eed16dfdd
Maaz Irshad Siddiqui <siddiqui.maaz&(@gmail.com>
2024-10-17 23:09:07 +0530

    DAY 1 PROJECT:Band Name Generator; UPDATE:made the final output variable in upper case

--- day1_pro/pro1_Band_Name_Generator.py
+++ day1_pro/pro1_Band_Name_Generator.py
@@ -2,5 +2,5 @@ print("-----------WELCOME TO BAND NAME GENERATOR-----------")
 city= input("What is name of the city you grew up ?:\n")
 pet= input("What is Your Pet's Name:\n")
 
-print(f"Your Band Name could be {city} {pet} ")
+print(f"Your Band Name could be {city.upper()} {pet.upper()}S ")
 print("-----------Thank You-----------")
