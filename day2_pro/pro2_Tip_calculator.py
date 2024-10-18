c95fc882b2175867d851a5d6ab8e3c580a5b27c6
Maaz Irshad Siddiqui <siddiqui.maaz&(@gmail.com>
2024-10-18 20:11:42 +0530

    Completed day2 of 100day python challange ::ADDED DAY 2 PROJECT:Tip & Bill Caculator

---
+++ day2_pro/pro2_Tip_calculator.py
@@ -0,0 +1,18 @@
+bill = int(input("Enter your total bill:"))
+
+
+tip = int(input("How much tip would you like to give? ['10','20','more']$:"))
+cgst =sgst = (bill*6)/100
+gst =cgst+sgst
+
+total_bill= round(float(bill+tip+gst),2)
+per_person = int (input("Enter How many people to split the bill:"))
+split = total_bill/per_person
+
+print("------------------------------RESTAURANT BILL------------------------------")
+print(f"\t\t Total Bill:{bill}")
+print(f"\t\t Tip:{tip}")
+print(f"\t\t TAX:{gst}")
+print(f"\t\t Total Bill+TAX+Tip:{total_bill}")
+print(f"\t\t Split among{per_person}:{split}")
+print("------------------------------PROGRAM EXECUTED------------------------------")
